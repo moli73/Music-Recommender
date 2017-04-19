2b9a36fd0bec79983b256fce2b2fb3c0f65723a5
moli73 <limo9373@gmail.com>
2017-04-18 22:08:09 -0500

    modify AlbumOut file format

--- data_preprocess/album_preprocess.py
+++ data_preprocess/album_preprocess.py
@@ -2,7 +2,7 @@ import sys
 def __album__preprocess__(input_filename, output_filename):
     '''
         This function is to split the album data into JSON file
-        The inputfile should in same directory of the python script    
+        The inputfile should in same directory of the python script
         python items_preprocess.py input_filename output_filename
     '''
     import re
@@ -20,9 +20,10 @@ def __album__preprocess__(input_filename, output_filename):
             for i in range(2, len(albumRecord)):
                 genreList.append(albumRecord[i])
             curAlbum['genreList'] = genreList
-            data.append(curAlbum.copy())
-    with open(output_filename, 'w') as wf:
-        json.dump(data, wf)
+            # data.append(curAlbum.copy())
+            with open(output_filename, 'a') as wf:
+                json.dump(curAlbum, wf)
+                wf.write('\n')
 #test albumData preprocess
-#__album__preprocess__("testAlbumIn.txt", "testAlbumOut.txt")
-__album__preprocess__(sys.argv[1], sys.argv[2])
+__album__preprocess__("testAlbumIn.txt", "testAlbumOut.json")
+# __album__preprocess__(sys.argv[1], sys.argv[2])

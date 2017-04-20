c15cfd32dc92a7e02d60a6c1342349ee719f9328
moli73 <limo9373@gmail.com>
2017-04-20 16:44:47 -0500

    arrange files

---
+++ data_preprocess/processor/album_preprocess.py
@@ -0,0 +1,29 @@
+import sys
+def __album__preprocess__(input_filename, output_filename):
+    '''
+        This function is to split the album data into JSON file
+        The inputfile should in same directory of the python script
+        python items_preprocess.py input_filename output_filename
+    '''
+    import re
+    import json
+    data = []
+    with open(input_filename, 'r') as f:
+        for line in f:
+            curAlbum = {}#the information of current album
+            albumRecord = re.split('\||\n', line)
+            if '' in albumRecord:
+                albumRecord.remove('')
+            curAlbum['albumID'] = albumRecord[0]
+            curAlbum['artistID'] = albumRecord[1]
+            genreList = []
+            for i in range(2, len(albumRecord)):
+                genreList.append(albumRecord[i])
+            curAlbum['genreList'] = genreList
+            # data.append(curAlbum.copy())
+            with open(output_filename, 'a') as wf:
+                json.dump(curAlbum, wf)
+                wf.write('\n')
+#test albumData preprocess
+__album__preprocess__("testAlbumIn.txt", "testAlbumOut.json")
+# __album__preprocess__(sys.argv[1], sys.argv[2])
---
+++ data_preprocess/processor/cutData.py
@@ -0,0 +1,11 @@
+import json
+def cutData(input_filename, output_filename, lines):
+    with open(input_filename, 'r') as f:
+        for i in range(lines):
+            curUserReview = json.loads(f.readline())
+            with open(output_filename, 'a') as wf:
+                json.dump(curUserReview, wf)
+                wf.write('\n')
+
+# cutData('trainOut.json', 'trainOut2w.json', 20000)
+cutData('testOut.json', 'testOut2w.json', 20000)

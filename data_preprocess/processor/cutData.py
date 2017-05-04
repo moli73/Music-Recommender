be770738e9b8cbb5e34fd96727227a628465bad7
moli73 <limo9373@gmail.com>
2017-05-03 20:16:10 -0500

    for merge

--- data_preprocess/processor/cutData.py
+++ data_preprocess/processor/cutData.py
@@ -8,4 +8,10 @@ def cutData(input_filename, output_filename, lines):
                 wf.write('\n')
 
 # cutData('trainOut.json', 'trainOut2w.json', 20000)
-cutData('testOut.json', 'testOut2w.json', 20000)
+# cutData('testOut.json', 'testOut2w.json', 20000)
+
+cutData('../trainOut.json', '../trainOut3w.json', 30000)
+cutData('../testOut.json', '../testOut3w.json', 30000)
+
+cutData('../trainOut.json', '../trainOut6w.json', 60000)
+cutData('../testOut.json', '../testOut6w.json', 60000)
